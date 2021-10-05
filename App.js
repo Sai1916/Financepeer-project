e8bbd95560a6dba4639d0850636493dc96f7db0a
Sai Sumedh <saisumedh16@gmail.com>
2021-10-05 20:08:20 +0530

    app

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import { Feather, Ionicons, MaterialIcons } from 'react-native-vector-icons'
 
 export default function App() {
+
+  const Tab = createBottomTabNavigator();
+
+  const Tabs= () => {
+    return(
+      <Tab.Navigator initialRouteName={HomeScreen}>
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={
+            {
+              headerShown: false,
+            }
+          }
+          tabBarIcon={<Feather name="home" size="24" />}
+        />
+        <Tab.Screen 
+          name="Movies" 
+          component={HomeScreen} 
+          options={
+            {
+              headerShown: false,
+            }
+          }
+          tabBarIcon={<Feather name="home" size="24" />}
+        />
+        <Tab.Screen 
+          name="Coming Soon" 
+          component={HomeScreen} 
+          options={
+            {
+              headerShown: false,
+            }
+          }
+          tabBarIcon={<Feather name="home" size="24" />}
+        />
+        <Tab.Screen 
+          name="Settings" 
+          component={HomeScreen} 
+          tabBarIcon={<Feather name="home" size="24" />}
+        />
+      </Tab.Navigator>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
